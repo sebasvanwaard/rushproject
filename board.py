44991a85b003a4680679d58f204432d3173400d4
S. Van Waard <s.vanwaard@sp-byods-145-109-20-46.wireless.uva.nl>
2024-06-05 11:49:54 +0200

    bug fix

--- board.py
+++ board.py
@@ -8,12 +8,16 @@ class Board:
     def __init__(self, shape, car_info):
         self.shape = shape
         self.car_info = car_info
-        self.cars = {}
+        self.cars = self.load_cars(car_info)
 
     def load_cars(self, car_info):
-        for car in car_info:
-            self.cars[car_info['car']] = Car(car_info['car'], car_info['col'], car_info['row'], car_info['length'],car_info['orientation'])
+        cars = {}
+
+        for _, car in car_info.iterrows():
+            cars[car['car']] = Car(car['car'], car['col'] - 1, car['row'] - 1, car['length'],car['orientation'])
     
+        return cars
+
     def draw(self, print_in_terminal = False):
         """
         Draw a numpy representation of the board
@@ -24,7 +28,7 @@ class Board:
         """
         board = np.full((self.shape, self.shape), '.')
 
-        for car in self.cars:
+        for car in self.cars.values():
             if car.orientation == 'H':
                 for i in range(car.length):
                     board[car.row, car.column + i] = car.name
