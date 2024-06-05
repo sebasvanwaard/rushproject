49f23cc1aaf58b883b5a80e94e528b2e56077936
anoukdv <anoukdv2000@gmail.com>
2024-06-05 11:40:21 +0200

    added move function in car.py

--- car.py
+++ car.py
@@ -4,12 +4,48 @@ import numpy as np
 
 
 class Car:
-	def __init__(self, name, x_pos, y_pos, length, orientation):
+	def __init__(self, name, column, row, length, orientation):
 		self.name = name
-		self.x_pos = x_pos
-		self.y_pos = y_pos
+		self.column = column
+		self.row = row
 		self.length = length
 		self.orientation = orientation
 
 	def move(self, steps):
-		pass
+		
+		# move vehicles with horizontal orientation
+		if self.orientation == H:
+
+			# dont move cars of the board
+			if self.length == 2:
+				if (self.column + steps) > 5 or (self.column + steps) < 1:
+					pass
+			
+			# dont move trucks of the board
+			if self.length == 3:
+				if (self.column + steps) > 4 or (self.column + steps) < 1:
+					pass
+			
+			# move the horizontal vehicle
+			else:
+				self.column += steps
+		
+		# move vehicles with vertical orientation
+		if orientation == V:
+
+			# dont move cars of the board
+			if self.length == 2:
+				if (self.row + steps) > 5 or (self.row + steps) < 1:
+					pass
+			
+			# dont move trucks of the board
+			if self.length == 3:
+				if (self.row + steps) > 4 or (self.row + steps) < 1:
+					pass
+			
+			# move the vertical vehicle
+			else:
+				self.row += steps
+		
+
+
