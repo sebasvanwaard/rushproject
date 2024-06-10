ec54758e9a1c1483dfe728e975b65c5e59343782
Sebas <sebasvanwaard@hotmail.com>
2024-06-10 18:16:53 +0200

    experimenten voor alle borden gerund

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 
 import run_experiment
 
-run_experiment.run_all_boards("gameboards", "experiments/random2", n_experiment=1, max_moves=10)
+run_experiment.run_all_boards("gameboards", "experiments/random2", n_experiment=1000, max_moves=250000)
 
 # parser = argparse.ArgumentParser(description = "filepath")
 # parser.add_argument("board", help = "input file (csv)")
