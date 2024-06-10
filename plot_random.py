b832bad2cf56c2be43d070c0eeaade35010e8e55
anoukdv <anoukdv2000@gmail.com>
2024-06-10 13:27:50 +0200

    plot_random.py inserted

---
+++ plot_random.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+def plot_random_csv(csv):
+    """
+    This function plots the distribution of the valid moves used for solving a board. 
+    """
+
+    df = pd.read_csv(csv)
+
+    print(df)
+
+    plt.hist(df['valid_moves'], bins=50, edgecolor='black')
+    plt.show()
+
+plot_random_csv('experiments/random/Rushhour6x6_1.csv')
