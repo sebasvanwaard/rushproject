fd8333f1732c1f9c500c08b6a26b8b2fe533699c
Sebas <sebasvanwaard@hotmail.com>
2024-06-11 12:13:43 +0200

    meer structuur in git nu yay

---
+++ code/game/board.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import argparse
+import numpy as np
+from car import Car
+
+
+class Board:
+    def __init__(self, shape, car_info):
+        self.shape = shape
+        self.car_info = car_info
+        self.cars = self.load_cars(car_info)
+
+    def load_cars(self, car_info):
+        cars = {}
+
+        for _, car in car_info.iterrows():
+            cars[car['car']] = Car(car['car'], car['col'] - 1, car['row'] - 1, car['length'],car['orientation'])
+
+        return cars
+
+    def draw(self, print_in_terminal = False):
+        """
+        Draw a numpy representation of the board
+        args:
+            print_in_terminal = print the board in the terminal
+        returns:
+            the numpy array of the board
+        """
+        self.board = np.full((self.shape, self.shape), '.')
+
+        for car in self.cars.values():
+            if car.orientation == 'H':
+                for i in range(car.length):
+                    self.board[car.row, car.column + i] = car.name
+            else:
+                for i in range(car.length):
+                    self.board[car.row + i, car.column] = car.name
+
+        if print_in_terminal:
+            print(self.board)
+
+        return self.board
--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 
-
-
-import run_experiment
+import rushproject.code.algorithms.run_experiment as run_experiment
 
 run_experiment.random_experiment("gameboards/Rushhour6x6_1.csv", "experiments/random", n_experiment=1)
 # run_experiment.run_all_boards("gameboards", "experiments/random", n_experiment=1000, max_moves=250000)
