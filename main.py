3500cfc9a2f6f4da0eccb568a2529180d9133e35
Sebas <sebasvanwaard@hotmail.com>
2024-06-13 13:08:24 +0200

    breadth_first aanpassingen

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 from src.experiment import *
 from src.game import *
 from src.algorithms import breadth_first
-# from src.algorithms import depth_first
+from src.algorithms import depth_first
 
 # test = Experiment("gameboards/Rushhour12x12_7.csv")
 
@@ -25,7 +25,7 @@ test_board = Board("gameboards/Rushhour6x6_1.csv")
 
 # test_algorithm = breadth_first.Breadth_first(test_board)
 test_algorithm = depth_first.Depth_first(test_board)
-final_board, total_moves, total_states = test_algorithm.run(max_depth=100)
+final_board, total_moves, total_states_used, total_states_generated = test_algorithm.run(max_depth=100)
 
 print(f"total moves: {total_moves}, total states used: {total_states_used}, total states generated: {total_states_generated}")
 final_board.plot()
--- src/algorithms/breadth_first.py
+++ src/algorithms/breadth_first.py
@@ -66,18 +66,20 @@ class Breadth_first(Algorithm):
 		possible_states.put(copy.deepcopy(self.board))
 
 		moves = 0
-		total_states = 0
+		total_states_used = 0
+		total_states_generated = 0
 		goal_state = self.board.shape - 2
 
 		while not possible_states.empty():
 				state = possible_states.get()
-				total_states += 1
+				total_states_used += 1
 				if state.cars["X"].x_pos == goal_state:
 					print("joepie")
-					return (state, moves, total_states)
+					return (state, state.depth, total_states_used, total_states_generated)
 
 				for possible_state in self.get_actions(state):
 					unique_id = possible_state.get_unique_id()
+					total_states_generated += 1
 					if unique_id not in self.state_archive:
 						self.state_archive.add(unique_id)
 						possible_states.put(possible_state)
@@ -85,3 +87,4 @@ class Breadth_first(Algorithm):
 		print(f"no solution found within {max_depth} moves")
 
 		return False
+	
