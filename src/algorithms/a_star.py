5e97dbe1d28cff33ff84bc68bfd41f75e67323eb
anoukdv <anoukdv2000@gmail.com>
2024-06-18 17:28:46 +0200

    delete old blocking blocking functions

--- src/algorithms/a_star.py
+++ src/algorithms/a_star.py
@@ -27,7 +27,7 @@ class A_star(Algorithm):
                 
                 possible_unique_id = possible_state.get_unique_id()
                 if possible_unique_id not in self.state_archive:
-                    state_cost_dict[possible_state] = possible_state.depth + self.red_car_cost(possible_state) + self.red_blocking_cost(possible_state)+ self.blocking_blocking_cost(possible_state)
+                    state_cost_dict[possible_state] = possible_state.depth + self.red_car_cost(possible_state) + self.red_blocking_cost(possible_state)+ self.calc_board_cost(possible_state)
                 
             state_cost_dict = dict(sorted(state_cost_dict.items(), key=lambda item: item[1], reverse=True))
             print(f"cost: {min(state_cost_dict.values())}")
@@ -52,70 +52,6 @@ class A_star(Algorithm):
                 cost += 1
         
         return cost
-
-    def blocking_blocking_cost(self, state):
-        blocking_cars = []
-        cost = 0
-
-        for i in range(state.cars["X"].x_pos + state.cars["X"].length, state.shape):
-            if state.grid[state.cars["X"].y_pos, i] != ".":
-                blocking_cars.append(state.grid[state.cars["X"].y_pos, i])
-
-        if len(blocking_cars) == 0:
-            return -500
-        
-        else:
-            for car in blocking_cars:
-                found_blocking_cars = []
-                temp_list = set()
-
-                while self.find_blocking_car(state, car):
-                    for i in self.find_blocking_car(state, car):
-                        temp_list.add(i)
-                   
-                    if len(found_blocking_cars) == 0:
-                        found_blocking_cars = temp_list
-                        temp_list = set()
-                        cost += 1
-
-                    car = found_blocking_cars.pop()
-
-        return cost
-                    
-    def find_blocking_car(self, state, car):
-        blocking_cars_ = []
-        if state.cars[car].orientation == 'H':
-            try:
-                blocking_car1 = state.grid[state.cars[car].y_pos, (state.cars[car].x_pos + state.cars[car].length)]
-                blocking_cars_.append(blocking_car1)
-            except IndexError:
-                pass
-
-            try:
-                blocking_car2 = state.grid[state.cars[car].y_pos, (state.cars[car].x_pos - 1)]
-                blocking_cars_.append(blocking_car2)
-            except IndexError:
-                pass
-
-        if state.cars[car].orientation == 'V':
-            try:
-                blocking_car1 = state.grid[(state.cars[car].y_pos + state.cars[car].length), state.cars[car].x_pos]
-                blocking_cars_.append(blocking_car1)
-            except IndexError:
-                pass
-
-            try:
-                blocking_car2 = state.grid[(state.cars[car].y_pos - 1), state.cars[car].x_pos]
-                blocking_cars_.append(blocking_car2)
-            except IndexError:
-                pass
-
-        if "." in blocking_cars_:
-            return False
-        else:                
-            return blocking_cars_
-
-
 
     def get_blocking_red(self, board):
         blocking_cars = []
