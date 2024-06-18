4eddd124b86ff726013d4a0fdda6fd82f90e2cc9
Sebas <sebasvanwaard@hotmail.com>
2024-06-18 10:52:47 +0200

    cost

--- src/algorithms/a_star.py
+++ src/algorithms/a_star.py
@@ -1,6 +1,7 @@
 from .algorithm import Algorithm
 
 import copy
+import math
 
 class A_star(Algorithm):
     def __init__(self, board):
@@ -13,6 +14,7 @@ class A_star(Algorithm):
         total_states_used = 0
         total_states_generated = 0
         state_cost_dict = {state: 0}
+
 
         while state.cars['X'].x_pos != goal_state:
             total_states_used += 1
@@ -66,6 +68,7 @@ class A_star(Algorithm):
             for car in blocking_cars:
                 found_blocking_cars = []
                 temp_list = set()
+
                 while self.find_blocking_car(state, car):
                     for i in self.find_blocking_car(state, car):
                         temp_list.add(i)
@@ -111,3 +114,61 @@ class A_star(Algorithm):
             return False
         else:                
             return blocking_cars_
+
+
+
+    def get_blocking_red(self, board):
+        blocking_cars = []
+
+        for i in range(board.cars["X"].x_pos + board.cars["X"].length, board.shape):
+            if board.grid[board.cars["X"].y_pos, i] != ".":
+                blocking_cars.append(board.grid[board.cars["X"].y_pos, i])
+
+        return set(blocking_cars)
+
+    def get_blocking_cars(self, board, car):
+        blocking_car1 = None
+        blocking_car2 = None
+
+        if car.orientation == 'H':
+            if car.x_pos + car.length < 6 and board.grid[car.y_pos, car.x_pos + car.length] != car:
+                blocking_car1 = board.grid[car.y_pos, car.x_pos + car.length]
+            if car.x_pos - 1 >= 0 and board.grid[car.y_pos, car.x_pos - 1] != car:
+                blocking_car2 = board.grid[car.y_pos, car.x_pos - 1]
+        if car.orientation == 'V':
+            if car.y_pos + car.length < 6 and board.grid[car.y_pos + car.length, car.x_pos] != car:
+                blocking_car1 = board.grid[car.y_pos + car.length, car.x_pos]
+            if car.y_pos - 1 >= 0 and board.grid[car.y_pos - 1, car.x_pos] != car:
+                blocking_car2 = board.grid[car.y_pos - 1, car.x_pos]
+        
+        return [blocking_car1, blocking_car2]
+
+
+    def calc_cost(self, board, car, cost):
+        if car == None or cost > len(board.cars):
+            return math.inf
+        
+        blocking_cars = self.get_blocking_cars(board, board.cars[car])
+        print(blocking_cars)
+       
+        if '.' in blocking_cars:
+            return cost
+        
+        cost = cost + 1
+
+        cost1 = self.calc_cost(board, blocking_cars[0], cost)
+        cost2 = self.calc_cost(board, blocking_cars[1], cost)
+
+        return min(cost1, cost2)
+
+    def calc_board_cost(self, board):
+        cars_blocking_red = self.get_blocking_red(board)
+        total_cost = 0
+        print(cars_blocking_red)
+
+        for car_blocking_red in cars_blocking_red:
+            total_cost += self.calc_cost(board, car_blocking_red, 0)
+            print(total_cost)
+            pass
+        
+        return total_cost
