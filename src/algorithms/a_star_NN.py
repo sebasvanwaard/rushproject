80b962030724f4d86f7b0abefd6f0d04f46c66dd
anoukdv <anoukdv2000@gmail.com>
2024-06-26 17:08:14 +0200

    strings added to a_star_NN

--- src/algorithms/a_star_NN.py
+++ src/algorithms/a_star_NN.py
@@ -8,11 +8,31 @@ from tensorflow.keras import models
 import time
 
 class A_star_nn(Algorithm):
+    """
+	This is a subclass of the class Algorithm and entails the A* algorithm. 
+	"""
+
     def __init__(self, board, model="src/neural_cost/models/board_cost_model_36x72x144x1.h5"):
+        """
+		The subclass A* with the neural network initializes everything from the parent class Algorithm and loads the neural network model used. 
+		"""
+
         super().__init__(board)
         self.cost_model = models.load_model(model)
 
     def run(self, max_time = math.inf):
+        """
+        run an A* search for a solution of the given board, with a neural network model to predict the moves necessary to solve the puzzle. 
+        Using a priority dictionary.
+        args:
+            max_time: maximum time to solve the puzzle before giving up. Default set to math.inf.
+		returns:
+			state: the board of the final state
+            state_depth: the depth of the board of the final state
+			total_states_used: the total states visited before the solution was found
+			total_states_generated: the total amount of states generated before the solution was found
+        """
+
         state = copy.deepcopy(self.board)
         goal_state = self.board.shape - 2
 
