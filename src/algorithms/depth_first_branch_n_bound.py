5ef12f0ac044f23496c17456de9929b264f0196f
anoukdv <anoukdv2000@gmail.com>
2024-06-26 15:58:25 +0200

    depth first branch n bound string

--- src/algorithms/depth_first_branch_n_bound.py
+++ src/algorithms/depth_first_branch_n_bound.py
@@ -6,10 +6,32 @@ import copy
 import time
 
 class Depth_first_branch_n_bound(Algorithm):
+    """
+	This is a subclass of the class Algorithm and entails the depth first algorithm with a branch and bound. 
+	"""
+
     def __init__(self, board):
+        """
+		The subclass Depth first with brach and bound initializes everything from the parent class Algorithm.
+		"""
+        
         super().__init__(board)
 
     def run(self, max_depth=math.inf, max_time=math.inf, n_goal_state = 200):
+        """
+        run a depth first search for a solution of the given board. It looks for the best solution by never looking deeper 
+        than the best solution already found. Using an integrated stack. 
+		args:
+			max_depth: the maximum depth the algorithm will search before giving up. Defaults to math.inf.
+            max_time: the maximum time the algorithm will search before giving up. Defaults to math.inf. 
+            n_goal_state: the maximum goal states one would visit. Defaults to 200. 
+		returns:
+			best_state: the board of the best final state
+            best_state_depth: the depth of the board of the best final state
+			total_states_used: the total states visited before the solution was found
+			total_states_generated: the total amount of states generated before the solution was found
+        """
+
         start_state = copy.deepcopy(self.board)
         stack = [start_state]
 
