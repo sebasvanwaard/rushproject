2c9a3ed7fca1b8a8e7a5fbd48d2baaa3127ee549
Sebas <sebasvanwaard@hotmail.com>
2024-06-26 17:08:36 +0200

    comments on depth first random

--- src/algorithms/depth_first_random_start.py
+++ src/algorithms/depth_first_random_start.py
@@ -11,6 +11,20 @@ class Depth_first_random_start(Algorithm):
         super().__init__(board)
 
     def run(self, max_random_choice_depth=0, max_depth=math.inf, max_time=math.inf):
+        """
+        run a depth first search for a solution of the given board. It looks for the best solution by never looking deeper 
+        than the best solution already found. Using an integrated stack. This variation on depth first search starts its search at a random
+        start state, in contrast to regular depth first (which always starts at the same state).
+		args:
+			max_depth: the maximum depth the algorithm will search before giving up. Defaults to math.inf.
+            max_time: the maximum time the algorithm will search before giving up. Defaults to math.inf. 
+            n_goal_state: the maximum goal states one would visit. Defaults to 200. 
+		returns:
+			best_state: the board of the best final state
+            best_state_depth: the depth of the board of the best final state
+			total_states_used: the total states visited before the solution was found
+			total_states_generated: the total amount of states generated before the solution was found
+        """
 
         start_state = copy.deepcopy(self.board)
         stack = [start_state]
