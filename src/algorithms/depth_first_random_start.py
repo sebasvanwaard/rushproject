b7112779d2346e7ddd9174158912d361606a4337
Sebas <sebasvanwaard@hotmail.com>
2024-06-26 16:23:42 +0200

    joepei

--- src/algorithms/depth_first_random_start.py
+++ src/algorithms/depth_first_random_start.py
@@ -8,30 +8,11 @@ import random
 import time
 
 class Depth_first_random_start(Algorithm):
-    """
-	This is a subclass of the class Algorithm and entails the depth first algorithm with a random start. 
-	"""
-
     def __init__(self, board):
-        """
-		The subclass Depth first with a random start initializes everything from the parent class Algorithm.
-		"""
-
         super().__init__(board)
 
     def run(self, max_random_choice_depth=0, max_depth=math.inf, max_time=math.inf):
-        """
-        run a depth first search for a solution of the given board. Integrated stack.
-		args:
-			max_random_choice_depth: the maximum random choice is the maximum of how many times a random choice is made. Defaults to 0. 
-            max_depth: the maximum depth the algorithm will search before giving up. Defaults to math.inf.
-            max_time: the maximum time the algorithm will search before giving up. Defaults to math.inf.
-        returns:
-			state: the board of the final state
-            state_depth: the depth of the board of the final state
-			total_states_used: the total states visited before the solution was found
-			total_states_generated: the total amount of states generated before the solution was found
-        """
+        goal_state = self.board.shape - 2
 
         start_state = copy.deepcopy(self.board)
         stack = [start_state]
@@ -40,7 +21,7 @@ class Depth_first_random_start(Algorithm):
 
         random_choice = 0
         possible_states = []
-        while len(stack) > 0 and start_time - time.time() < max_time:
+        while len(stack) > 0 and time.time() - start_time < max_time:
             state = stack.pop()
             self.total_states_used += 1
 
